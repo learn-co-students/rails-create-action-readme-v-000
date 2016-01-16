f407cc95d5bb1346500f1ae79cdbfacb5a348d56
Lukeghenco <lukeghenco@gmail.com>
2016-01-16 09:10:53 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,10 @@ class PostsController < ApplicationController
 	def new
 		@post = Post.new
 	end
+
+	def create
+		@post = Post.create(title: params[:title], description: params[:description])
+		@post.save
+		redirect_to post_path(@post) # or use 'posts_path' if you wanted to go to index page.
+	end
 end
