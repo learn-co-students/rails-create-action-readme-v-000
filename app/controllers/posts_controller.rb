c37d76c1c812acc387904f1bc99373de6be0fd47
umohm1 <umohm12@gmail.com>
2017-04-18 20:19:05 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,15 @@
 class PostsController < ApplicationController
+
 	def index
 		@posts = Post.all
 	end
 
 	def show
-		@post = Post.find(params[:id])
+	  @post = Post.find(params[:id])
 	end
 
 	def new
-		@post = Post.new
+	  @post = Post.new
 	end
 
 	def create
