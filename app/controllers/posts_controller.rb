8ec2cab3a33e8d8ed582931d7781344ea0207fbe
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-04-04 15:34:41 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,11 @@ class PostsController < ApplicationController
 		@post = Post.find(params[:id])
 	end
 
-	def new
+	def create
 		@post = Post.new
+		@post.title = params[:title]
+		@post.description = params[:description]
+		@post.save
+		redirect_to post_path(@post)
 	end
 end
