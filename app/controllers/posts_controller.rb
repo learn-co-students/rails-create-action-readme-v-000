5b240224657918ba0acf98bb6e4a4cd7c17f23ba
Aubrey Neimeier <neimeier.1@osu.edu>
2018-04-25 17:30:45 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,15 @@ class PostsController < ApplicationController
 	def new
 		@post = Post.new
 	end
+
+	def create
+		#communicate w/ database and redirect
+		@post = Post.new
+	  @post.title = params[:title]
+	  @post.description = params[:description]
+	  @post.save
+		#can check in console with Post.last
+		redirect_to post_path(@post)
+		#redirect to show page
+	end
 end
