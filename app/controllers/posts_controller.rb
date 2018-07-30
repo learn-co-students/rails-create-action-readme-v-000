c0575f19193c985c97f5ab76dbf5bc77522f707a
arye-dov-eidelman <aryedov.e@gmail.com>
2018-07-30 15:12:30 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,8 @@ class PostsController < ApplicationController
 	def new
 		@post = Post.new
 	end
+
+	def create
+		redirect_to Post.create(title: params['title'], description: params['description']) 
+	end
 end
