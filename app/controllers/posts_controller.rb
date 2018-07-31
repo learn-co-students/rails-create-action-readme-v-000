1dc36d9bc959fbaf132c442cfa4ef1a635da1f46
github username <github email address>
2018-07-31 16:58:29 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,9 +9,6 @@ class PostsController < ApplicationController
 
 	def new
 		@post = Post.new
-		@post.title = params[:title]
-		@post.description = params[:description]
-		@post.save
 		redirect_to post_path(@post)
 	end
 end
