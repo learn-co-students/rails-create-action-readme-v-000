3d8a39f171fd51bf0208f4ca4f51ded4c07a5732
Ashley Wong <ashwong04@gmail.com>
2019-03-28 16:36:56 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
     @post.title = params[:title]
     @post.description = params[:description]
     @post.save
-    redirect_to post_path(@post)
+    redirect_to posts_path(@post)
   end
 
 end
